6584167816f5dcf6ff620f714ee236cf3c744e68
Andre Carlos <amarques@a5solutions.com>
2022-11-17 22:36:19 -0300

    Removido subtitle

--- src/app/components/menu-app-bar/LanguageSetting.tsx
+++ src/app/components/menu-app-bar/LanguageSetting.tsx
@@ -1,63 +1,28 @@
-import { FormControlLabel, FormGroup, ListItem, ListItemText, Switch, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import { ListItem, ListItemText, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import { LanguageTypes, useLanguageContext } from "../../context";
 import { MouseEvent, useCallback } from "react";
-import { languageValues, useLanguageContext } from "../../context";
 
-interface ILanguageSettingProps {
-    isLanguage?: boolean;
-}
+export const LanguageSetting = () => {
 
-export const LanguageSetting = ({ isLanguage = false }: ILanguageSettingProps) => {
+    const options: LanguageTypes[] = ['pt', 'en'];
 
-    const options: languageValues[] = ['pt', 'en'];
+    const { language, toggleLanguage } = useLanguageContext();
 
-    const { language, subtitle, toggleLanguage, toggleSubtitle } = useLanguageContext();
-
-    const handleLanguage = useCallback((event: MouseEvent<HTMLElement>, value: languageValues) => {
+    const handleLanguage = useCallback((event: MouseEvent<HTMLElement>, value: LanguageTypes) => {
         toggleLanguage(value);
-    }, [language])
-
-    const handleSubtitle = useCallback((event: MouseEvent<HTMLElement>, value: languageValues) => {
-        toggleSubtitle(value);
-    }, [subtitle])
-
-    if (isLanguage)
-        return (
-            <ListItem sx={{ flexDirection: 'column' }}>
-                <ListItemText>
-                    <Typography variant="subtitle2">
-                        Idioma
-                    </Typography>
-                </ListItemText>
-                <ToggleButtonGroup
-                    value={language}
-                    exclusive
-                    onChange={handleLanguage}
-                    size={'small'}
-                >
-                    {
-                        options.map((opt) => (
-                            <ToggleButton key={opt} value={opt}>
-                                <Typography variant="caption">
-                                    {opt}
-                                </Typography>
-                            </ToggleButton>
-                        ))
-                    }
-                </ToggleButtonGroup>
-            </ListItem>
-        )
+    }, []);
 
     return (
         <ListItem sx={{ flexDirection: 'column' }}>
             <ListItemText>
                 <Typography variant="subtitle2">
-                    Legenda
+                    Idioma
                 </Typography>
             </ListItemText>
             <ToggleButtonGroup
-                value={subtitle}
+                value={language}
                 exclusive
-                onChange={handleSubtitle}
+                onChange={handleLanguage}
                 size={'small'}
             >
                 {
